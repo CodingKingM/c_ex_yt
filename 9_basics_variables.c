d68abda401201e3f78cbd8362455a1aff9f0d0c1
Malek <malek.alsibai@dynamics-regensburg.de>
2024-03-24 06:38:24 +0100

    2 arguments

--- 9_basics_variables.c
+++ 9_basics_variables.c
@@ -7,6 +7,6 @@ x = 10; // variable initalization
 
 int y = x / 2;  // example of expression (x/2)
 printf("Hello World\n");   // statement
-printf("The value of y is %d\n", y);
+printf("The value of y is %i\nThe value of x is %i", y, x);   // 2 arguments y and x seperated by a comma
     return 0;
 }
