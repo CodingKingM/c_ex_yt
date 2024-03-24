858765e17f46e6a5531bc069853f9292adc439f2
Malek <malek.alsibai@dynamics-regensburg.de>
2024-03-24 06:36:10 +0100

    expression

--- 9_basics_variables.c
+++ 9_basics_variables.c
@@ -5,8 +5,8 @@ int main(){
 int x; // varible declaration
 x = 10; // variable initalization
 
-int y = x / 2;
-printf("Hello World\n");
+int y = x / 2;  // example of expression (x/2)
+printf("Hello World\n");   // statement
 printf("The value of y is %d\n", y);
     return 0;
 }
