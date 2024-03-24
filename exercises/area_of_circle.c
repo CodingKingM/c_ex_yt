61e926ae8b4fa8f004c318d71f5085bbdb55ec90
Malek <malek.alsibai@dynamics-regensburg.de>
2024-03-24 06:45:13 +0100

    working code but only integer

--- exercises/area_of_circle.c
+++ exercises/area_of_circle.c
@@ -2,5 +2,10 @@
 
 int main(){
 
+printf("Welcome! Please enter a radius\n");
+int radius;
+scanf("%i", &radius);
+int area = 3.14 * (radius * radius);
+printf("The area of the circle is %i given the radius %i\n", area, radius);
     return 0;
 }
