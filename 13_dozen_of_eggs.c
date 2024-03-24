76ca193eb277235c8c1004ed0f421b2946990045
Malek <malek.alsibai@dynamics-regensburg.de>
2024-03-24 07:11:14 +0100

    working code using TYPE CASTING

--- 13_dozen_of_eggs.c
+++ 13_dozen_of_eggs.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 // a dozen = 12 eggs
-int main (){
+int main()
+{
 
-printf("Welcome! Please enter the amount of eggs for today\n");
-int eggs;
-scanf("%d", &eggs);
-
+    printf("Welcome! Please enter the amount of eggs for today\n");
+    int eggs;
+    scanf("%d", &eggs);
+    double dozen = (double) eggs / 12; // (double) was used as type casting
+    printf("The number of eggs produced today is %i which equal to %f dozen\n", eggs, dozen);
     return 0;
-
 }
